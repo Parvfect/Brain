fc48f74aea5e17c1bfc0c2cd7ac1e582b552ef7f
Parv <parvagrw02@gmail.com>
2021-01-25 14:12:06 +0530

    Cool we have begun

---
+++ useful_methods.py
@@ -0,0 +1,6 @@
+"""
+To do 
+Operator overloading for arrays, definition of math and basic 
+stuff we can use
+
+"""
---
+++ visualisation.py
@@ -0,0 +1,9 @@
+"""
+
+Functions to help visulaise available data, 
+phase plots etc
+
+I would also like for it to be able to handle the more mathy stuff like 
+visualising functions and what not
+
+"""
