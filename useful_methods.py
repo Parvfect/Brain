6b485cfba69e2d0d599f54d08b2fa591a1acb44a
Parv <parvagrw02@gmail.com>
2021-02-10 11:19:03 +0530

    Hurrah?

--- useful_methods.py
+++ useful_methods.py
@@ -4,3 +4,11 @@ Operator overloading for arrays, definition of math and basic
 stuff we can use
 
 """
+
+#I swear to god I feel like I have written representations for all of these
+class Matrix:
+
+    def __init__(self, values):
+        
+        for i in values:
+            self.matrix[i][j] = values[i+j+k]
